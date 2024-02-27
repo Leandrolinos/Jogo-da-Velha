7ace49a82fac78301e17c170ec841f4e6d9deebd
Leandrolinos <leandro.alves.oliveira@gmail.com>
2024-02-27 17:42:47 -0300

    Primeiro commit com o jogo da velha

---
+++ js/game.js
@@ -0,0 +1,56 @@
+const board = document.getElementById('board');
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+
+function createCell(index) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.dataset.index = index;
+    cell.addEventListener('click', handleCellClick);
+    return cell;
+}
+
+function renderBoard() {
+    board.innerHTML = '';
+    gameBoard.forEach((value, index) => {
+        const cell = createCell(index);
+        cell.textContent = value;
+        board.appendChild(cell);
+    });
+}
+
+function handleCellClick(event) {
+    const index = event.target.dataset.index;
+    if (gameBoard[index] === '' && !checkWinner()) {
+        gameBoard[index] = currentPlayer;
+        renderBoard();
+        if (checkWinner()) {
+            alert(`Player ${currentPlayer} venceu!`);
+        } else if (!gameBoard.includes('')) {
+            alert('Empate!');
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
+}
+
+function checkWinner() {
+    const winPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    return winPatterns.some(pattern =>
+        gameBoard[pattern[0]] !== '' &&
+        gameBoard[pattern[0]] === gameBoard[pattern[1]] &&
+        gameBoard[pattern[1]] === gameBoard[pattern[2]]
+    );
+}
+
+renderBoard();
